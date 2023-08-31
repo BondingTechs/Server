5c5f42e70a1dd02e3b86fda6f6ec16fe2fc02143
brian chang <zz02846584zz@gmail.com>
2023-09-01 02:19:03 +0800

    fix: 留言板修正、ip API修正

--- dist/modules/news/controller/app/comment.d.ts
+++ dist/modules/news/controller/app/comment.d.ts
@@ -6,14 +6,6 @@ import { NewsCommentApiService } from '../../service/app/comment';
 export declare class NewsCommentApiController extends BaseController {
     newsCommentService: NewsCommentApiService;
     /**
-     * 分頁
-     * @param param
-     */
-    getPage(query: any): Promise<{
-        code: import("@cool-midway/core").RESCODE;
-        message: import("@cool-midway/core").RESMESSAGE;
-    }>;
-    /**
      * 新增
      * @param param
      */
