62516ca0ffd475165aa36d5ba25aee460b1ad39a
BondingTechs <139861052+BondingTechs@users.noreply.github.com>
2024-02-20 17:16:20 +0800

    update twilo token

--- dist/config/credentials.js
+++ dist/config/credentials.js
@@ -4,7 +4,7 @@ exports.googleMail = exports.twilio = void 0;
 exports.twilio = {
     // serviceSid: 'VA37ac49d0caa3e4c20f1e0ab02b46476a',
     accountSid: 'ACf5f58da6bc9b4cc4892d13fdcf82b3e9',
-    authToken: '4693b13358e5bb887ac760b4e71ff8bb',
+    authToken: '4a0f876cf85ac0e218d9135259b2046f',
     verifySid: 'VAf7f0f6acda0fb5244de1f2876adaba59',
 };
 exports.googleMail = {
